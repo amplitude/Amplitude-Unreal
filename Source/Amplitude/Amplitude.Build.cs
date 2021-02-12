da98cd98b70ad2e8cb9cc45422ba16e03a9c2082
John Tran <jptran318@gmail.com>
2021-02-11 17:12:00 -0800

    remove unnecessary build configs

--- Source/Amplitude/Amplitude.Build.cs
+++ Source/Amplitude/Amplitude.Build.cs
@@ -5,9 +5,9 @@ using Tools.DotNETCommon;
 
 public class Amplitude : ModuleRules
 {
-	public Amplitude(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+  public Amplitude(ReadOnlyTargetRules Target) : base(Target)
+  {
+    PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
     string LibraryPath = Path.GetFullPath(Path.Combine(ModuleDirectory, "../Library"));
     Log.TraceInformation("In Amplitude.Build.cs");
     Log.TraceInformation(LibraryPath);
@@ -16,100 +16,45 @@ public class Amplitude : ModuleRules
     {
       PrivateIncludePaths.AddRange(
         new string[] {
-          // Path.Combine(LibraryPath, "iOS", "Headers")
           Path.Combine(LibraryPath, "iOS", "x86_64", "Amplitude.framework", "Headers")
-          // ... add public include paths required here ...
         }
       );
       PublicIncludePaths.AddRange(
         new string[] {
           Path.Combine(LibraryPath, "iOS", "x86_64", "Amplitude.framework", "Headers")
-          // ... add public include paths required here ...
         }
       );
       PublicFrameworks.Add(Path.Combine(LibraryPath, "iOS", "x86_64", "Amplitude.framework"));
-          
-      PublicFrameworks.AddRange(
-          new string[] {
-              "AdSupport",
-              "SystemConfiguration"
-          }
-      );
-    } else
+    }
+    else
     {
       throw new NotImplementedException(Target.Platform.ToString() + " is not a supported target platform by the Amplitude Unreal SDK plugin");
     }
 
-    // PrivateIncludePaths.AddRange(
-    //   new string[] {
-    //     Path.Combine(LibraryPath, "iOS")
-    //     // ... add public include paths required here ...
-    //   }
-    // );
-          
+    PrivateIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "Private")));
+    PrivateIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "Public")));
+    PublicIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "Public")));
 
-		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
-			}
-			);
-				
-		
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
-			}
-			);
-      // var ModulePath = Path.GetDirectoryName(RulesCompiler.GetModuleFilename(this.GetType().Name));
-      // Log.TraceInformation(ModuleDirectory);
+    PublicDependencyModuleNames.AddRange(
+      new string[]
+      {
+        "Core",
+      }
+      );
 
-      PrivateIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "Private")));
-      PrivateIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "Public")));
-      PublicIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "Public")));
-			
-		
-		PublicDependencyModuleNames.AddRange(
-			new string[]
-			{
-				"Core",
-        "CoreUObject",
-        "Engine",
-        "Amplitude",
-				// ... add other public dependencies that you statically link with here ...
-			}
-			);
-			
-		
-		PrivateDependencyModuleNames.AddRange(
-			new string[]
-			{
-				// "Projects",
-				// "InputCore",
-				// "UnrealEd",
-        // "Analytics",
-				// "ToolMenus",
-				// "CoreUObject",
-				// "Engine",
-				// "Slate",
-				// "SlateCore",
-        "Amplitude",
-				// ... add private dependencies that you statically link with here ...	
-			}
+
+    PrivateDependencyModuleNames.AddRange(
+      new string[]
+      {
+        "Analytics",
+      }
     );
-    
+
     PublicIncludePathModuleNames.AddRange(
       new string[]
       {
-          "Analytics",
-          "Engine"
+        "Analytics",
       }
     );
-		
-		DynamicallyLoadedModuleNames.AddRange(
-			new string[]
-			{
-				// ... add any modules that your module loads dynamically here ...
-			}
-			);
-	}
+  }
 }
