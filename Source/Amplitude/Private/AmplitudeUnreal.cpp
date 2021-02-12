416fa3f9fcd299e1058ea3acb74fd5c5eeeb3cf5
John Tran <jptran318@gmail.com>
2021-02-11 17:07:26 -0800

    Implement IAnalyticsProvider interface

--- Source/Amplitude/Private/AmplitudeUnreal.cpp
+++ Source/Amplitude/Private/AmplitudeUnreal.cpp
@@ -1,15 +1,98 @@
 #include "AmplitudeUnreal.h"
 #include "AmplitudeProvider.h"
+#include "Analytics.h"
+#if PLATFORM_APPLE
+  #include "AmplitudeiOSBridge.h"
+#endif
 
-static const FName AmplitudeTabName("Amplitude");
+IMPLEMENT_MODULE(FAmplitudeUnreal, Amplitude)
 
-#define LOCTEXT_NAMESPACE "FAmplitudeUnreal"
+TSharedPtr<IAnalyticsProvider> FAmplitudeProvider::AmplitudeProvider;
+
+void FAmplitudeUnreal::StartupModule()
+{
+
+}
+
+void FAmplitudeUnreal::ShutdownModule()
+{
+  FAmplitudeProvider::Destroy();
+}
 
 TSharedPtr<IAnalyticsProvider> FAmplitudeUnreal::CreateAnalyticsProvider(const FAnalyticsProviderConfigurationDelegate &GetConfigValue) const 
 {
-  return MakeShareable(new FAmplitudeProvider());
+	if (GetConfigValue.IsBound())
+	{
+		const FString Key = GetConfigValue.Execute(TEXT("AmplitudeApiKey"), true);
+		return FAmplitudeProvider::Create(Key);
+	}
+	else
+	{
+		UE_LOG(LogAnalytics, Warning, TEXT("AmplitudeUnreal::CreateAnalyticsProvider was called with an unbound config delegate"));
+	}
+	return nullptr;
 }
 
-#undef LOCTEXT_NAMESPACE
+FAmplitudeProvider::FAmplitudeProvider(const FString Key) : ApiKey(Key)
+{
 
-IMPLEMENT_MODULE(FAmplitudeUnreal, Amplitude)
+}
+FAmplitudeProvider::~FAmplitudeProvider()
+{
+	if (bHasSessionStarted)
+	{
+		EndSession();
+	}
+}
+
+
+bool FAmplitudeProvider::StartSession(const TArray<FAnalyticsEventAttribute>& Attributes)
+{
+  #if PLATFORM_APPLE
+    std::string ConvertedApiKey = std::string(TCHAR_TO_UTF8(*ApiKey));
+    ios_bridge::AmplitudeiOSBridge bridge;
+    bridge.initializeApiKey(ConvertedApiKey);
+  #endif
+  bHasSessionStarted = true;
+  return bHasSessionStarted;
+}
+
+void FAmplitudeProvider::EndSession()
+{
+  bHasSessionStarted = false;
+}
+
+void FAmplitudeProvider::RecordEvent(const FString& EventName, const TArray<FAnalyticsEventAttribute>& Attributes)
+{
+  #if PLATFORM_APPLE
+    ios_bridge::AmplitudeiOSBridge bridge;
+    std::string ConvertedEventName = std::string(TCHAR_TO_UTF8(*EventName));
+    UE_LOG(LogAnalytics, Warning, TEXT("Amplitude event logged - %s"), *EventName);
+    bridge.logEvent(ConvertedEventName);
+  #endif
+}
+
+FString FAmplitudeProvider::GetSessionID() const
+{
+  return TEXT("NO-OP");
+}
+
+bool FAmplitudeProvider::SetSessionID(const FString& InSessionID)
+{
+  return true;
+}
+
+void FAmplitudeProvider::FlushEvents()
+{
+  
+}
+
+void FAmplitudeProvider::SetUserID(const FString& InUserID)
+{
+  
+}
+
+FString FAmplitudeProvider::GetUserID() const
+{
+  return TEXT("NO-OP");
+}
