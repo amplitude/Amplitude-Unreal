a2fd24bae5f0397860b92963ffaebd32e8bbefa7
John Tran <jptran318@gmail.com>
2021-03-15 11:24:01 -0700

    format nits

--- Source/Amplitude/Public/AmplitudeUnreal.h
+++ Source/Amplitude/Public/AmplitudeUnreal.h
@@ -7,39 +7,38 @@
 /*
  * The public interface to this module
  */
-class FAmplitudeUnreal :
-	public IAnalyticsProviderModule
+class FAmplitudeUnreal : public IAnalyticsProviderModule
 {
-	/** Singleton for analytics */
-	TSharedPtr<IAnalyticsProvider> AmplitudeProvider;
+  /** Singleton for analytics */
+  TSharedPtr<IAnalyticsProvider> AmplitudeProvider;
 
-	//--------------------------------------------------------------------------
-	// Module functionality
-	//--------------------------------------------------------------------------
+  //--------------------------------------------------------------------------
+  // Module functionality
+  //--------------------------------------------------------------------------
 public:
-	/**
+  /**
 	 * Singleton-like access to this module's interface.  This is just for convenience!
 	 * Beware of calling this during the shutdown phase, though.  Your module might have been unloaded already.
 	 *
 	 * @return Returns singleton instance, loading the module on demand if needed
 	 */
-	static inline FAmplitudeUnreal& Get()
-	{
-		return FModuleManager::LoadModuleChecked< FAmplitudeUnreal >( "Amplitude" );
-	}
+  static inline FAmplitudeUnreal &Get()
+  {
+    return FModuleManager::LoadModuleChecked<FAmplitudeUnreal>("Amplitude");
+  }
 
-	//--------------------------------------------------------------------------
-	// provider factory functions
-	//--------------------------------------------------------------------------
+  //--------------------------------------------------------------------------
+  // provider factory functions
+  //--------------------------------------------------------------------------
 public:
-	/**
+  /**
 	 * IAnalyticsProviderModule interface.
 	 * Creates the analytics provider given a configuration delegate.
 	 * The keys required exactly match the field names in the Config object. 
 	 */
-	virtual TSharedPtr<IAnalyticsProvider> CreateAnalyticsProvider(const FAnalyticsProviderConfigurationDelegate& GetConfigValue) const override;
-	
+  virtual TSharedPtr<IAnalyticsProvider> CreateAnalyticsProvider(const FAnalyticsProviderConfigurationDelegate &GetConfigValue) const override;
+
 private:
-	virtual void StartupModule() override;
-	virtual void ShutdownModule() override;
+  virtual void StartupModule() override;
+  virtual void ShutdownModule() override;
 };
